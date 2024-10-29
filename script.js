b107f32d45b59bc70f60fe4745484b24199e685b
klapkunie <158785252+klapkunie@users.noreply.github.com>
2024-10-29 14:56:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ function nextPage() {
         document.getElementById('page1').style.transform = 'rotateY(-180deg)';
         document.getElementById('page2').style.transform = 'rotateY(0deg)';
         currentPage = 2;
+    } else if (currentPage === 2) {
+        document.getElementById('page2').style.transform = 'rotateY(-180deg)';
+        document.getElementById('page3').style.transform = 'rotateY(0deg)';
+        currentPage = 3;
     }
 }
 
 function prevPage() {
-    if (currentPage === 2) {
-        document.getElementById('page1').style.transform = 'rotateY(0deg)';
+    if (currentPage === 3) {
+        document.getElementById('page3').style.transform = 'rotateY(-360deg)';
         document.getElementById('page2').style.transform = 'rotateY(-180deg)';
+        currentPage = 2;
+    } else if (currentPage === 2) {
+        document.getElementById('page2').style.transform = 'rotateY(0deg)';
+        document.getElementById('page1').style.transform = 'rotateY(0deg)';
         currentPage = 1;
     }
 }
